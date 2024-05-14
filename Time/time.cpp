f3057ecc1f0263447574050f3e154b48af820241
Michael Slice <mjsens523@gmail.com>
2024-05-14 00:26:43 -0500

    Fixing Time

--- Time/time.cpp
+++ Time/time.cpp
@@ -10,14 +10,8 @@ using namespace std::chrono;
 
 auto Time::get_current_time()
 {   
-    auto start = std::chrono::system_clock::now();
-    auto end = std::chrono::system_clock::now();
+    auto const time = std::chrono::current_zone()
 
-    std::chrono::duration<double> elapsed_second = end - start;
 
-    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
-
-    std::cout << "Finished Computation at " << std::ctime(&end_time)
-    << "elapsed seconds " << elapsed_seconds.count() << "s" << std::endl; 
 };
-
+ 
