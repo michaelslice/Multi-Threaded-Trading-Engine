37cadcc47776f4dd8f7e5634fd284ea9f00f23b3
Michael Slice <mjsens523@gmail.com>
2024-05-15 17:42:51 -0500

    Added OrderId Creation

--- OrderId/orderid.cpp
+++ OrderId/orderid.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
+
+#include <chrono>
+#include <ctime>
+#include <cstdlib>
+
 #include "orderid.h"
 
 
 void OrderId::get_OrderId()
 {
+    auto now = std::chrono::system_clock::now();
+    std::time_t now_time = std::chrono::system_clock::to_time_t(now);
     
+    std::srand(time(&now_time)); // Use to current time as the seed value
+    this->OrderId = std::rand();
 }
--- Orderbook/orderbook.h
+++ Orderbook/orderbook.h
@@ -1,14 +1,23 @@
 #ifndef ORDERBOOK_H
 #define ORDERBOOK_H
 #include <iostream>
+
+#include <algorithm>
+#include <map>
+#include <vector>
+
 #include "../Order/order.h"
 
 class Orderbook
 {
 private:
+    std::map<double, std::vector<Order>> buyOrders;
+    std::map<double, std::vector<Order>> sellOrders;
 public:
     Orderbook();
     void ValidOrderDay();
+    void AddOrder();
+    
 };
 
 #endif
